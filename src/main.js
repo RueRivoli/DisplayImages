3a712f562c84f65f8b49c418ea66135b651aa7af
Florent <flgallois@gmail.com>
2020-09-26 21:49:07 +0200

    CORRECTION: REMOVE AXIOS IMPORT

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import router from './router/routes.js'
@@ -8,7 +7,6 @@ import store from './store/'
 import './style/style.css'
 
 Vue.config.productionTip = false
-Vue.use(axios);
 Vue.use(VueRouter);
 Vue.use(Vuex);
 
